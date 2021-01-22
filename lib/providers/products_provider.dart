393ed785b85356401621740448daafec1a76d135
nitai <nitaibarran5@gmail.com>
2021-01-22 00:07:32 -0400

    reached lecture 195

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/providers/product.dart';
 
-class ProductsProvider with ChangeNotifier{
-
+class ProductsProvider with ChangeNotifier {
   // List<Product> _items = [];
 
   List<Product> _items = [
@@ -12,7 +11,7 @@ class ProductsProvider with ChangeNotifier{
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
       imageUrl:
-      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'p2',
@@ -20,7 +19,7 @@ class ProductsProvider with ChangeNotifier{
       description: 'A nice pair of trousers.',
       price: 59.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'p3',
@@ -28,7 +27,7 @@ class ProductsProvider with ChangeNotifier{
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
       imageUrl:
-      'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'p4',
@@ -36,7 +35,7 @@ class ProductsProvider with ChangeNotifier{
       description: 'Prepare any meal you want.',
       price: 49.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
 
@@ -45,8 +44,7 @@ class ProductsProvider with ChangeNotifier{
     return [..._items]; // this is returning a copy of the items
   }
 
-  Product findById(String id){
+  Product findById(String id) {
     return _items.firstWhere((product) => product.id == id);
   }
-
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shop/models/product.dart';
+import 'file:///C:/Users/nitai/nitai-projects/shop/lib/providers/product.dart';
 import 'package:shop/widgets/product_item.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/products_provider.dart';
 
 class ProductsGrid extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     //it is important to note that this provider.of(context) would
     //return the provider that is supplied in the parent widget
     //of the widget tree. and only the child widgets of this widget
@@ -19,16 +17,16 @@ class ProductsGrid extends StatelessWidget {
     final products = productsData.items; // this is fucking amazing lol
 
     return GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: products.length,
-        itemBuilder: (context, i) => ProductItem(products[i].id,
-            products[i].title, products[i].imageUrl),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      );
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (context, i) =>
+          ProductItem(products[i].id, products[i].title, products[i].imageUrl),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
   }
 }
